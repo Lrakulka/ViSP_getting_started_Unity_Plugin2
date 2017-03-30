f336fea4902aeeca37679367b725f594a3ebc9ff
Lrakulka <olecsandr32@gmail.com>
2017-03-30 18:01:09 +0300

    Test environment

---
+++ UnityViSPpluginTest/Assets/Scripts/InformationHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationHandler : MonoBehaviour {
+
+	public Text cube3Dposition;
+	public Text pluginOutput;
+
+	private Vector3 privPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (privPosition != transform.position) {
+			privPosition = transform.position;
+			UpdateUI ();
+		}
+	}
+
+	private void UpdateUI() {
+		cube3Dposition.text = transform.position.ToString ();
+	}
+}
---
+++ UnityViSPpluginTest/Assets/Scripts/Teleport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Method teleports game object if it fell out of the game scene
+public class Teleport : MonoBehaviour {
+
+	public Vector3 startPosition;
+
+	public float teleportTriggerY = -10;
+
+	void Update () {
+		if (gameObject.transform.position.y < -10) {
+				gameObject.transform.position = startPosition;
+			}
+	}
+}
